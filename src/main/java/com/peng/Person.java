ebaf478303bb46e58336e87b2bf6f969aa0637b6
changshengjianke <lancaozhunweirui@gmail.com>
2021-08-19 16:33:42 +0800

    123

---
+++ src/main/java/com/peng/Person.java
@@ -0,0 +1,11 @@
+package com.peng;
+
+/**
+ * @author
+ * @date 2021/8/19 16:32
+ */
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
